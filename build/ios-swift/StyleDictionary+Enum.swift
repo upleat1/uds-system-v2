d992d8eb67deb0f5466adda157fb2f542b8b8cf0
Your Namenaya100 <esbaek2@gmail.com>
2025-04-04 16:57:28 +0900

    commit 1

---
+++ build/ios-swift/StyleDictionary+Enum.swift
@@ -0,0 +1,66 @@
+
+//
+// StyleDictionary+Enum.swift
+//
+
+// Do not edit directly, this file was auto-generated.
+
+
+import UIKit
+
+public enum StyleDictionaryEnum {
+    public static let bordersHeavy = [object Object]
+    public static let bordersStylesDashed = [object Object]
+    public static let bordersStylesSolid = solid
+    public static let bordersWireframe = [object Object]
+    public static let colorsBlack = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let colorsOrange100 = UIColor(red: 1.000, green: 0.980, blue: 0.941, alpha: 1)
+    public static let colorsOrange200 = UIColor(red: 0.996, green: 0.922, blue: 0.784, alpha: 1)
+    public static let colorsOrange300 = UIColor(red: 0.984, green: 0.827, blue: 0.553, alpha: 1)
+    public static let colorsOrange400 = UIColor(red: 0.965, green: 0.678, blue: 0.333, alpha: 1)
+    public static let colorsOrange500 = UIColor(red: 0.929, green: 0.537, blue: 0.212, alpha: 1)
+    public static let colorsOrange600 = UIColor(red: 0.867, green: 0.420, blue: 0.125, alpha: 1)
+    public static let colorsOrange700 = UIColor(red: 0.753, green: 0.337, blue: 0.129, alpha: 1)
+    public static let colorsOrange800 = UIColor(red: 0.612, green: 0.259, blue: 0.129, alpha: 1)
+    public static let colorsOrange900 = UIColor(red: 0.482, green: 0.204, blue: 0.118, alpha: 1)
+    public static let colorsWhite = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let dimensions0 = CGFloat(0.00)
+    public static let dimensions1 = CGFloat(64.00)
+    public static let dimensions10 = CGFloat(640.00)
+    public static let dimensions11 = CGFloat(704.00)
+    public static let dimensions12 = CGFloat(768.00)
+    public static let dimensions13 = CGFloat(832.00)
+    public static let dimensions14 = CGFloat(896.00)
+    public static let dimensions15 = CGFloat(960.00)
+    public static let dimensions2 = CGFloat(128.00)
+    public static let dimensions3 = CGFloat(192.00)
+    public static let dimensions4 = CGFloat(256.00)
+    public static let dimensions5 = CGFloat(320.00)
+    public static let dimensions6 = CGFloat(384.00)
+    public static let dimensions7 = CGFloat(448.00)
+    public static let dimensions8 = CGFloat(512.00)
+    public static let dimensions9 = CGFloat(576.00)
+    public static let dimensionsMax = CGFloat(159984.00)
+    public static let shadowsLg = [object Object]
+    public static let shadowsMulti = [object Object],[object Object]
+    public static let shadowsSm = [object Object]
+    public static let textFontsSans = Open Sans, sans-serif
+    public static let textFontsSerif = Times New Roman, serif
+    public static let textLineHeightsLarge = 1.8
+    public static let textLineHeightsNormal = 1.2
+    public static let textSizesBody = CGFloat(256.00)
+    public static let textSizesHeading = CGFloat(448.00)
+    public static let textTypographyBody = [object Object]
+    public static let textTypographyHeading = [object Object]
+    public static let textWeightsBold = extra-bold
+    public static let textWeightsLight = thin
+    public static let textWeightsRegular = regular
+    public static let transitionsDurationsInstant = 0ms
+    public static let transitionsDurationsLong = 600ms
+    public static let transitionsDurationsMedium = 300ms
+    public static let transitionsDurationsShort = 100ms
+    public static let transitionsEasingFunctionsAccelerate = 0.5,0,1,1
+    public static let transitionsEasingFunctionsDecelerate = 0,0,0.5,1
+    public static let transitionsEmphasis = [object Object]
+    public static let transitionsFade = [object Object]
+}
---
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -0,0 +1,66 @@
+
+//
+// StyleDictionary+Struct.swift
+//
+
+// Do not edit directly, this file was auto-generated.
+
+
+import UIKit
+
+internal struct StyleDictionaryStruct {
+    internal static let bordersHeavy = [object Object]
+    internal static let bordersStylesDashed = [object Object]
+    internal static let bordersStylesSolid = solid
+    internal static let bordersWireframe = [object Object]
+    internal static let colorsBlack = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    internal static let colorsOrange100 = UIColor(red: 1.000, green: 0.980, blue: 0.941, alpha: 1)
+    internal static let colorsOrange200 = UIColor(red: 0.996, green: 0.922, blue: 0.784, alpha: 1)
+    internal static let colorsOrange300 = UIColor(red: 0.984, green: 0.827, blue: 0.553, alpha: 1)
+    internal static let colorsOrange400 = UIColor(red: 0.965, green: 0.678, blue: 0.333, alpha: 1)
+    internal static let colorsOrange500 = UIColor(red: 0.929, green: 0.537, blue: 0.212, alpha: 1)
+    internal static let colorsOrange600 = UIColor(red: 0.867, green: 0.420, blue: 0.125, alpha: 1)
+    internal static let colorsOrange700 = UIColor(red: 0.753, green: 0.337, blue: 0.129, alpha: 1)
+    internal static let colorsOrange800 = UIColor(red: 0.612, green: 0.259, blue: 0.129, alpha: 1)
+    internal static let colorsOrange900 = UIColor(red: 0.482, green: 0.204, blue: 0.118, alpha: 1)
+    internal static let colorsWhite = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    internal static let dimensions0 = CGFloat(0.00)
+    internal static let dimensions1 = CGFloat(64.00)
+    internal static let dimensions10 = CGFloat(640.00)
+    internal static let dimensions11 = CGFloat(704.00)
+    internal static let dimensions12 = CGFloat(768.00)
+    internal static let dimensions13 = CGFloat(832.00)
+    internal static let dimensions14 = CGFloat(896.00)
+    internal static let dimensions15 = CGFloat(960.00)
+    internal static let dimensions2 = CGFloat(128.00)
+    internal static let dimensions3 = CGFloat(192.00)
+    internal static let dimensions4 = CGFloat(256.00)
+    internal static let dimensions5 = CGFloat(320.00)
+    internal static let dimensions6 = CGFloat(384.00)
+    internal static let dimensions7 = CGFloat(448.00)
+    internal static let dimensions8 = CGFloat(512.00)
+    internal static let dimensions9 = CGFloat(576.00)
+    internal static let dimensionsMax = CGFloat(159984.00)
+    internal static let shadowsLg = [object Object]
+    internal static let shadowsMulti = [object Object],[object Object]
+    internal static let shadowsSm = [object Object]
+    internal static let textFontsSans = Open Sans, sans-serif
+    internal static let textFontsSerif = Times New Roman, serif
+    internal static let textLineHeightsLarge = 1.8
+    internal static let textLineHeightsNormal = 1.2
+    internal static let textSizesBody = CGFloat(256.00)
+    internal static let textSizesHeading = CGFloat(448.00)
+    internal static let textTypographyBody = [object Object]
+    internal static let textTypographyHeading = [object Object]
+    internal static let textWeightsBold = extra-bold
+    internal static let textWeightsLight = thin
+    internal static let textWeightsRegular = regular
+    internal static let transitionsDurationsInstant = 0ms
+    internal static let transitionsDurationsLong = 600ms
+    internal static let transitionsDurationsMedium = 300ms
+    internal static let transitionsDurationsShort = 100ms
+    internal static let transitionsEasingFunctionsAccelerate = 0.5,0,1,1
+    internal static let transitionsEasingFunctionsDecelerate = 0,0,0.5,1
+    internal static let transitionsEmphasis = [object Object]
+    internal static let transitionsFade = [object Object]
+}
